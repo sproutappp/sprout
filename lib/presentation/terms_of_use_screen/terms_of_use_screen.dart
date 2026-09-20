ccd902f8e4ef03354934fb5ffbbdfb2cfdc4b47e
mishramaheshmaddy <mahesh.gkp@gmail.com>
2026-09-20 13:35:50 +0530

    Add Sprout terms of use screen

---
+++ lib/presentation/terms_of_use_screen/terms_of_use_screen.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../theme/app_theme.dart';
+
+class TermsOfUseScreen extends StatelessWidget {
+  const TermsOfUseScreen({super.key});
+
+  static const sections = <Map<String, String>>[
+    {
+      'title': 'Sprout Terms of Use',
+      'body': 'These Terms of Use (“Terms”) govern your use of our website located at https://akshatmedia.in and the Sprout mobile application (collectively, the “Platform”) made available by Akshat Media (“Sprout”, “Company”, “we”, “us” and “our”), a private company established under the laws of India. The terms “you” and “your” refer to the user of the Platform.\n\nThese Terms are to be read with the Sprout Privacy Policy. If you do not agree with the terms and conditions of these Terms, please do not use this Platform.\n\nOur Services and these Terms are compliant with the Indian Penal Code, 1860, and Information Technology Act, 2000, including all amendments made to it and rules framed under it. When you create an account on our Platform or use our Platform or any of our Services, you accept and agree to these Terms. However, please note that we do not represent that we are compliant with laws of any country apart from the Republic of India. If you wish to use our Services, please ensure that you are permitted to do so, in your jurisdiction.\n\nYou and we are required to follow certain rules while you use our Platform. We have listed these rules in these Terms. Please read these Terms and all other hyperlinks mentioned here carefully. Do remember that by using our Platform, you agree to these Terms. Also, if you are using these services outside India, please comply with your local laws.'
+    },
+    {
+      'title': 'CHANGES TO TERMS AND SERVICES',
+      'body': 'Our Platform is dynamic and may change rapidly. As such, we may change the services we provide at our discretion. We may temporarily, or permanently, stop providing Services or any features to you generally.\n\nWe may remove or add functionalities to our Platform and Services without any notice. However, if we make a change where your consent is required, we will make sure to ask for it. Please be sure to keep visiting this page from time to time to stay updated on our latest changes and developments.'
+    },
+    {
+      'title': 'WHO MAY USE OUR SERVICES',
+      'body': 'Our Platform helps you stay in touch with your friends and community or your organisation and enables you to share images, videos, audios and other updates. We understand your preferred content and personalize your newsfeed to show you posts, pictures, videos, and suggest content available on our Platform (“Service/Services”).\n\nYou may use our Services only if you are capable of forming a binding agreement with us and are legally permitted to use our Services. If you are accepting these Terms on behalf of a company or any legal persons, then you represent and warrant that you have the authority to bind such entity to these Terms and effectively “you” and “your” shall refer to the company.\n\nPlease ensure that you are allowed to use our services under the law.'
+    },
+    {
+      'title': 'HOW TO USE OUR SERVICES',
+      'body': 'Sprout is an app which brings you friends and community closer to you and allows you to share your content. To use our Services, you must register on our Platform by entering your phone number and the One-Time-Password sent by us via SMS to your phone number. We do not read any information stored on your mobile device and computer without your permission.'
+    },
+    {
+      'title': 'PRIVACY POLICY',
+      'body': 'To effectively provide and introduce any new Services to you, we collect certain information such as your phone number and your name from you. We may further request and store additional information. Such information is stored securely on amazon web services or “AWS” cloud servers and the “Google Cloud Platform” cloud servers, thereby also subject to the terms of the AWS and Google Cloud privacy policy. The Sprout Privacy Policy explains how we collect, use, share and store the information collected. Furthermore it also details your rights under law and how you may control the data you provide us.\n\nYou provide us with some information about yourself so that we can serve you better. We have described how we store and use this information in the Sprout Privacy Policy.'
+    },
+    {
+      'title': 'YOUR COMMITMENTS',
+      'body': 'Providing a safe Service for a broad audience requires that we all do our part. In return for our commitment to provide our Services, we require you to make some commitments to us.\n\n1. No Impersonation or False Information to be Provided\nYou are required to use your actual name on our Platform, you are required to input your correct phone number and gender to use our Services. You will not falsely represent yourself as another person or representative of another person to use our Services. You will not lie about your details, including your age, for any reason.\n\n2. Device Security\nWe have implemented measures to ensure that our Platform is secure. However, there is no guarantee that our Platform is immune to virus attacks. You will ensure that you have requisite anti-malware and antivirus software on your mobile device and computer to ensure its safety. You will not allow any person to use your phone number, and not allow multiple accounts to be linked to your phone number. You will be responsible for all content posted by any account linked to your phone number.\n\n3. Content Removal and Termination\nYour usage of our Platform is governed by the Sprout Terms of Use. If any of our users report your content to be against the Sprout Terms of Use, we may remove such content from our Platform. In the event that multiple reports are made regarding violation of the Sprout Terms of Use, we will terminate your account from our platform and block you from registering with us. If you wish to appeal any such removal, you may write to us at contact@akshatmedia.in.\n\n4. Platform Not to be Used For Anything Unlawful or Illegal\nYou shall not use our Platform to share any content which is obscene, pornographic, harmful for minors, discriminatory, spreading hate speech, inciting any form of violence or hatred against any persons, or of seditious in nature, or violates any laws of the Republic of India, or is barred from being shared by any laws of the Republic of India. We reserve the right to remove such content.\n\nIn addition to the above, please note that we may share your information with appropriate law enforcement authorities if we have good-faith belief that it is reasonably necessary to share your personal data or information in order to comply with any legal obligation; or to protect the rights or prevent any harm to our property or safety, our customers, or public; or to detect, prevent or otherwise address public safety, fraud, security or technical issues. You understand however, that we cannot be held responsible for any actions done by or to you by a third party or user by way of using our Platform. Refrain from sharing any content which is illegal or causes any harm to the well-being of members of the society or community.'
+    },
+    {
+      'title': '5. Content Rights and Liabilities',
+      'body': 'We strongly believe in the freedom of expression and allow you to share photographs, images, videos, music, other updates, and other content on our Platform. We do not have any ownership over any of the content shared by you and the rights in the content remain only with you. You will not use our Platform to violate or infringe upon our or any third-party’s intellectual property rights. Such content is against Sprout Terms of Use and may be removed from the Platform. Further, if you use any content developed by us, then we shall continue to own the intellectual property rights in such content.\n\nBy sharing/posting/uploading content using our Services, you grant us a non-exclusive, royalty-free, transferable, sub-licensable, worldwide license to host, use, distribute, run, copy, publicly perform or display, translate, and create derivative works of your content (consistent with your privacy and application settings).\n\nYou may request to delete your content and/or account at any point. However, your content may continue to appear on the Platform if it has been shared with others. To learn more about how we use information, and how to control or delete your content, please read the Sprout Privacy Policy.\n\nYou remain solely responsible for the content you post on our Platform. We do not endorse and are not responsible for any content shared or posted on or through our Platform, and for the consequence of such sharing or posting. The presence of our logo or any trademark on any content shared by you does not mean that we have endorsed or sponsored your content. Further, we will not be liable for or responsible for the consequences of any transactions made or entered into by you with other users of the Platform. You will always have ownership and responsibilities for the content you share. We will never claim that we have intellectual property rights over your content, but will have a free of cost, permanent license to use what you share and post on our Platform.'
+    },
+    {
+      'title': '6. Intermediary Status and No Liability',
+      'body': 'We are an intermediary under the Information Technology Act, 2000 and the Information Technology Act (Intermediary Guidelines) Rules, 2011. These Terms are published in accordance with the provisions of Rule 3(1) of the Information Technology (Intermediaries Guidelines) Rules, 2011 that require publishing of the rules and regulations, Sprout Privacy Policy, and Sprout Terms of Use for accessing and using our Platform. Our role is limited to providing a platform to display the content shared by you and our other users.\n\nWe do not control what people do or say and are not responsible for their (or your) actions (whether online or offline). We are not responsible for services and features offered by others, even if you access them through our Services. Our responsibility for anything that happens on our Platform is strictly governed by the laws of the Republic of India and is limited to that extent. You agree that we will not be responsible for any loss of profits, revenues, information, or data, or consequential, special, indirect, exemplary, punitive, or incidental damages arising out of or related to these Terms, even if we know they are possible. This includes when we delete your content, information, or account. We are an intermediary under Indian law. We do not control what people post on our Platform but we expect everyone to comply with the Sprout Terms of Use.'
+    },
+    {
+      'title': '7. You Will Not Attempt to Disrupt or Jeopardize Sprout',
+      'body': 'We have developed a Photo Sharing and community-driven platform. Therefore, you agree to not interfere with, or use non-public areas of our Platform, Services, and our technical delivery system. You will not introduce any trojans, viruses, any other malicious software, any bots or scrape our Platform for any user information. Additionally, you will not probe, scan, or test the vulnerability of any system, security or authentication measures implemented by us. If you tamper or attempt to tamper with our technological design and architecture, we may terminate your user profile. You will not hack into or introduce malicious software of any kind onto our Platform. If you commit such actions, we may remove you from the platform and We may further report such actions to the appropriate law enforcement authorities and proceed against you with legal actions.'
+    },
+    {
+      'title': 'PERMISSIONS YOU GIVE TO US',
+      'body': 'You accept these Terms and give us certain permissions so that we can serve you better. Permissions you have granted us are:\n\n1. Automatic Downloads and Updates\nWe are constantly updating our Platform and Services offered. To use our Platform, you may need to download the Sprout mobile application to your mobile device and update it from time to time.\n\n2. Permission to Use Cookies\nWe may use cookies, pixel tags, web beacons, mobile device IDs, flash cookies and similar files or technologies to collect and store information with respect to your use of the Services and third-party websites. Please see the Sprout Cookie Policy for more information regarding the use of cookies and other technologies described in this section, including regarding your choices relating to such technologies. All websites use cookies and store them on your web browser so that usage information can be stored and logged in your browser.\n\n3. Data Retention\nYou grant us the right to store and retain information relating to you and provided by you. We shall have the right to retain certain information regarding your usage of the Platform. Please view the Sprout Privacy Policy for further information relating to the collection, storage and use of your information by us.'
+    },
+    {
+      'title': 'OUR AGREEMENT AND WHAT HAPPENS IF WE DISAGREE',
+      'body': '1. Who Has Rights Under These Terms\nThe rights and obligations under these terms are granted only to you and shall not be assigned to any third party without our consent. However, we are permitted to assign our rights and obligations under these Terms to others. This can happen when, for example, we enter into a merger with another company and create a new company.\n\n2. How We Will Handle Disputes\nIn all cases, you agree that disputes will be subject to the laws of the Republic of India and the courts of Bangalore shall have exclusive jurisdiction over all such disputes.\n\n3. Grievance Officer\nWe have a Grievance Officer to address your concerns regarding data safety, privacy, and Platform usage concerns. We will resolve the issues raised by you within 30 (thirty) days from receiving them. You may contact the Grievance Officer at the following Email:\nEmail: contact@akshatmedia.in'
+    },
+    {
+      'title': 'LIMITATION OF LIABILITY',
+      'body': 'We do not assume any liability with respect to any loss or damage, arising directly or indirectly due to any inaccuracy or incompleteness of any information or a breach of any warranty or guaranty due to the actions of any user of the Platform.\n\nThe Platform and Services, are provided on “as is” and “as available” basis without any representation or warranties, express or implied except otherwise specified in writing. We do not warrant the quality of the Services or the Platform including its uninterrupted, timely, secure or error-free provision, continued compatibility on any device, or correction of any errors.\n\nIn no event shall we, or any of our affiliates, successors, and assigns, and each of their respective investors, directors, officers, employees, agents, service providers, and suppliers be liable for any special, incidental, punitive, direct, indirect or consequential damages suffered as a consequence of a breach of the Terms by another user or arising out of the use of or the reliance on any of the Services or the Platform.\n\nIn the event any exclusion contained herein is held to be invalid for any reason and we or any of our affiliate entities, officers, directors or employees become liable for loss or damage, then, any such liability shall be limited to not exceeding the charges or amounts paid to us for use of the Platform or the Services in the month preceding the date of the claim.'
+    },
+    {
+      'title': 'INDEMNIFICATION',
+      'body': 'You agree to indemnify, defend and hold harmless us, and our subsidiaries, affiliates and agents and their respective officers, directors, employees, successors and assigns from and against any claim, proceeding, loss, damage, liability, cost, demand or expense (including but not limited to attorney’s fees) of any kind arising out of: (i) your access to or use of the Platform and Services; (ii) any breach by you of your obligations under this Agreement; (iii) your violation of the rights of any third party, including any infringement of intellectual property, or of any privacy or consumer protection right; (iv) any violation of law or contractual obligation and any claims, demands, notices pursuant to such violation; (v) your negligence or willful misconduct. This obligation will survive the termination of our Terms.'
+    },
+    {
+      'title': 'UNSOLICITED MATERIAL',
+      'body': 'We always appreciate feedback or other suggestions. We may use the same without any restrictions or obligation to compensate you for them and are under no obligation to keep them confidential.'
+    },
+    {
+      'title': 'GENERAL',
+      'body': '1. If any aspect of these Terms is unenforceable, the rest will remain in effect.\n\n2. Any amendment or waiver to our Terms must be in writing and signed by us.\n\n3. If we fail to enforce any aspect of these Terms, including reporting any illegal or impermissible actions to appropriate law enforcement authorities or blocking or suspending your profile, such failure to enforce our rights will not be a waiver by us.\n\n4. We reserve all rights not expressly granted to you.'
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: AppTheme.backgroundDark,
+        appBar: AppBar(
+          backgroundColor: AppTheme.backgroundDark,
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_rounded),
+            color: AppTheme.textPrimary,
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text(
+            'Terms of Use',
+            style: GoogleFonts.manrope(
+              color: AppTheme.textPrimary,
+              fontSize: 18,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.fromLTRB(20, 16, 20, 40),
+          children: [
+            Text(
+              'Sprout Terms of Use',
+              style: GoogleFonts.manrope(
+                color: AppTheme.textPrimary,
+                fontSize: 24,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Please read these Terms of Use together with the Sprout Privacy Policy before using the Platform.',
+              style: GoogleFonts.manrope(
+                color: AppTheme.textSecondary,
+                fontSize: 14,
+                height: 1.6,
+              ),
+            ),
+            const SizedBox(height: 24),
+            for (final section in sections) ...[
+              Text(
+                section['title']!,
+                style: GoogleFonts.manrope(
+                  color: AppTheme.textPrimary,
+                  fontSize: 17,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                section['body']!,
+                style: GoogleFonts.manrope(
+                  color: AppTheme.textSecondary,
+                  fontSize: 14,
+                  height: 1.65,
+                ),
+              ),
+              const SizedBox(height: 24),
+            ],
+          ],
+        ),
+      );
+}
